1c43db33aa5e0b316b0b51abdc9d98d39a5a1b94
Liam McNamara <liam.mcnamara@klarna.com>
2016-09-28 16:27:57 +0200

    tmux back end

--- setup_config.sh
+++ setup_config.sh
@@ -3,4 +3,11 @@
 git config --global user.email "liam.mcnamara@klarna.com"
 git config --global user.name "Liam McNamara"
 
+# Install useful software
 yum -y install vim tmux
+
+# Copy dot files
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}"  )" && pwd  )"
+cp -r $DIR/files/* ~
+
+
