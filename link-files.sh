3ec9a65ac6359664dc72f468ce53184e288d6a15
Liam McNamara <liam.mcnamara@klarna.com>
2016-11-12 21:44:03 +0100

    link files is more verbose

--- link-files.sh
+++ link-files.sh
@@ -6,13 +6,12 @@ FILES=`ls -a $DIR/files`
 for f in $FILES; do
     SOURCE="$DIR/files/$f"
     DEST="$HOME/$f"
-    #if [ -f $SOURCE ]; then
     if [ ! -e $DEST ]; then
         echo "ln -s $SOURCE $DEST";
         ln -s $SOURCE $DEST;
+    else
+        echo "Warning: not making link, file already exists: $DEST"
     fi
-    #else
-    #fi
 done;
 
 
