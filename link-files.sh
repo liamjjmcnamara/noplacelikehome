a90324c4ec3e4018cf34d3e6522f8a4ac1904529
Liam McNamara <liam.mcnamara@klarna.com>
2016-11-12 13:43:16 +0100

    cleaning up

--- link-files.sh
+++ link-files.sh
@@ -1,4 +1,19 @@
+#!/bin/bash -e
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}"  )" && pwd  )"
+FILES=`ls -a $DIR/files`
+
+for f in $FILES; do
+    SOURCE="$DIR/files/$f"
+    DEST="$HOME/$f"
+    #if [ -f $SOURCE ]; then
+    if [ ! -e $DEST ]; then
+        echo "ln -s $SOURCE $DEST";
+        ln -s $SOURCE $DEST;
+    fi
+    #else
+    #fi
+done;
 
 
 
